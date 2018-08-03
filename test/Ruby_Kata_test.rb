9e214d43b7b28c9327e57fe30b97d642b8965249
Will Krause <wkrause1@radford.edu>
2018-08-03 15:06:38 +0000

    Using class variable in test file

--- test/Ruby_Kata_test.rb
+++ test/Ruby_Kata_test.rb
@@ -1,19 +1,31 @@
 require 'test_helper'
 
 class RubyKataTest < Minitest::Test
-  def test_gutter_game
-    game = RubyKata.new
-    20.times do
-      game.roll(0)
+  @@game = RubyKata.new
+  private
+  def rollMany(rolls, pins)
+    rolls.times do
+      @@game.roll(pins)
     end
-    assert_equal(0, game.score)
+  end
+  def setUp
+    @@game = RubyKata.new
+  end
+
+  public
+  def test_gutter_game
+    setUp
+    20.times do
+      @@game.roll(0)
+    end
+    assert_equal(0, @@game.score)
   end
 
   def test_all_ones
-    game = RubyKata.new
+    setUp
     20.times do
-      game.roll(1)
+      @@game.roll(1)
     end
-    assert_equal(20, game.score)
+    assert_equal(20, @@game.score)
   end
 end
