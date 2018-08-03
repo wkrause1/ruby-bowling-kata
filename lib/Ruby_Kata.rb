28e89fe5c67370365a8508746e3b2fad04bf5b84
Will Krause <wkrause1@radford.edu>
2018-08-03 16:15:27 +0000

    Refactoring

--- lib/Ruby_Kata.rb
+++ lib/Ruby_Kata.rb
@@ -2,14 +2,44 @@ require "Ruby_Kata/version"
 
 class RubyKata
   def initialize
-    @score = 0
+    @rolls = Array.new(21, 0)
+    @current_roll = 0
   end
 
   def roll(pins)
-    @score += pins
+    @rolls[@current_roll] = pins
+    @current_roll += 1
   end
 
   def score
-    @score
+    score = 0
+    frame_index = 0
+    10.times do
+      if is_spare(frame_index)
+        score += 10 + @rolls[frame_index+2]
+        frame_index += 2
+      else
+      score += @rolls[frame_index] + @rolls[frame_index+1]
+      frame_index += 2
+        end
+    end
+    return score
+  end
+
+  private
+  def sum_balls_in_frame(frame_index)
+    @rolls[frame_index] + @rolls[frame_index + 1]
+  end
+
+  def spare_bonus(frame_index)
+    @rolls[frame_index + 2]
+  end
+
+  def strike_bonus(frame_index)
+    @rolls[frame_index + 1] + @rolls[frame_index + 2]
+  end
+
+  def is_spare(frame_index)
+    @rolls[frame_index] + @rolls[frame_index + 1] ==10
   end
 end
--- test/Ruby_Kata_test.rb
+++ test/Ruby_Kata_test.rb
@@ -3,29 +3,47 @@ require 'test_helper'
 class RubyKataTest < Minitest::Test
   @@game = RubyKata.new
   private
-  def rollMany(rolls, pins)
+  def roll_many(rolls, pins)
     rolls.times do
       @@game.roll(pins)
     end
   end
-  def setUp
+
+  def set_up
     @@game = RubyKata.new
+  end
+
+  def roll_spare
+    2.times {@@game.roll(5)}
   end
 
   public
   def test_gutter_game
-    setUp
-    20.times do
-      @@game.roll(0)
-    end
+    set_up
+    roll_many(20, 0)
     assert_equal(0, @@game.score)
   end
 
   def test_all_ones
-    setUp
-    20.times do
-      @@game.roll(1)
-    end
+    set_up
+    roll_many(20, 1)
     assert_equal(20, @@game.score)
+  end
+
+  def test_one_spare
+    set_up
+    roll_spare
+    @@game.roll(3)
+    roll_many(17, 0)
+    assert_equal(16, @@game.score)
+  end
+
+  def test_one_strike
+    set_up
+    @@game.roll(10)
+    @@game.roll(3)
+    @@game.roll(4)
+    roll_many(16,0)
+    assert_equal(24, @@game.score)
   end
 end
